33618be6f49e9330de62cdc53c2d13512ba84d3f
devklick <devklick@gmail.com>
2024-07-28 06:35:25 +0100

    Cache GET spins response

--- api/skins.ts
+++ api/skins.ts
@@ -1,8 +1,10 @@
 import { getSkins } from "../src/u-he/u-he-webservice";
 
+const cacheDuration = 1000 * 60 * 60 * 24 * 7; // 1 week
+
 export async function GET(request: Request) {
   const skins = await getSkins();
   return new Response(JSON.stringify(skins), {
-    headers: [["Content-Type", "application/json"]],
+    headers: [["Cache-Control", `public, s-maxage=${cacheDuration}`]],
   });
 }
